464e58e58b2f1274f2ca63975b129ecfe360a982
suji5262 <suji5262@gmail.com>
2023-09-20 17:55:13 +0900

    :sparkles: Ex15 file add

---
+++ Part2/part2/Ex15.java
@@ -0,0 +1,14 @@
+package part2;
+
+public class Ex15 {
+    public static void main(String[] args) {
+
+        // 기본 타입의 종류
+        boolean isL = false;
+        char suit = 'H';
+        int year = 2023;
+        double latitude = 37.52127220511242;
+        double longitude = 127.0074462890625;
+        System.out.println(latitude + "\t" + longitude);
+    }
+}
